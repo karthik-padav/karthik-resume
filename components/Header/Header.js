21cce716e9a1f4b751e6dffa01c6bc7b0b798a1e
karthik-padav <karthikpadav@gmail.com>
2024-03-23 13:42:02 +0530

    error resolved

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -55,9 +55,9 @@ export default function Header({ menu, activeTab }) {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M1 1h15M1 7h15M1 13h15"
               />
             </svg>
