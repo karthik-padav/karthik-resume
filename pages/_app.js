25a70fabdf6bcd62599d5d68589587be99cd624c
karthikpadav <karthikpadav@gmail.com>
2024-07-08 19:14:38 +0530

    GOOGLE ANALYTICS

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
+import Script from "next/script";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="gtm-script-2">
+        {`  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS})`}
+      </Script>
       <Component {...pageProps} />
     </>
   );
