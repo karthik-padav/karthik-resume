ae0bd935e6e1f9df58cf48b9aa22c161e42dcba0
karthikpadav <karthikpadav@gmail.com>
2024-07-08 19:05:54 +0530

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+      />
+      <Script id="gtm-script-2">
+        {`  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', ${process.env.GOOGLE_ANALYTICS})`}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
